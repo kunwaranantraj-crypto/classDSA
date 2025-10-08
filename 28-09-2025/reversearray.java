96d6010e8f58a5a90cf71c76285adbd61b0852b0
kunwaranantraj-crypto <kunwaranantraj@gmail.com>
2025-10-08 11:53:27 +0530

    Update and rename q2.java to reversearray.java

---
+++ 28-09-2025/reversearray.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            int t = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = t;
+        }
+
+        for (int x : arr) System.out.print(x + " ");
+    }
+}
