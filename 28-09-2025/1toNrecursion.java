6a75b77de4632b7beeabc7466a5718758f803eba
kunwaranantraj-crypto <kunwaranantraj@gmail.com>
2025-10-08 12:00:32 +0530

    Update and rename q8.java to 1toNrecursion.java

---
+++ 28-09-2025/1toNrecursion.java
@@ -0,0 +1,11 @@
+class Main {
+    static void print(int n) {
+        if (n == 0) return;
+        print(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        print(10);
+    }
+}
