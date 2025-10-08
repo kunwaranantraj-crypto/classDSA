5ed79eba360c30b8111f6bb736886cf364e7aea7
kunwaranantraj-crypto <kunwaranantraj@gmail.com>
2025-10-08 12:01:46 +0530

    Update and rename q9.java to sumofn natural no.java

---
+++ 28-09-2025/sumofn natural no.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 10, s = 0;
+
+        for (int i = 1; i <= n; i++) s += i;
+
+        System.out.println("Sum = " + s);
+    }
+}
